2876d5c9aded7acf4d7f27ff1315ea88db9ddd5d
Pete Canavan <canavanpm@gmail.com>
2016-12-24 14:53:08 -0500

    added a new "n/"

--- app/src/main/java/example/pete/com/girhubproject/ItemDetailActivity.java
+++ app/src/main/java/example/pete/com/girhubproject/ItemDetailActivity.java
@@ -31,6 +31,7 @@ public class ItemDetailActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own detail action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                "n"
             }
         });
 
